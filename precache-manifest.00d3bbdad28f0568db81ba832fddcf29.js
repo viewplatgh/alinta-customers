7a93f6e224d2be66928e8854915ab12a4776983f
Rob L <viewpl@gmail.com>
2019-09-04 08:05:36 +1000

    Updates

---
+++ precache-manifest.00d3bbdad28f0568db81ba832fddcf29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c919d2d46425247fc8985f6e292aae",
+    "url": "/alinta-customers/index.html"
+  },
+  {
+    "revision": "b977ea3a756b2e8e2e9f",
+    "url": "/alinta-customers/static/css/2.2a42eaee.chunk.css"
+  },
+  {
+    "revision": "571d857f61eff6411613",
+    "url": "/alinta-customers/static/css/main.e1bb867e.chunk.css"
+  },
+  {
+    "revision": "b977ea3a756b2e8e2e9f",
+    "url": "/alinta-customers/static/js/2.32da382b.chunk.js"
+  },
+  {
+    "revision": "571d857f61eff6411613",
+    "url": "/alinta-customers/static/js/main.38ca1494.chunk.js"
+  },
+  {
+    "revision": "261b2978f372e6ef78bb",
+    "url": "/alinta-customers/static/js/runtime~main.7f3a5d78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f8a580cba7fed052f41618b736118c0.js"
+  "/alinta-customers/precache-manifest.00d3bbdad28f0568db81ba832fddcf29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alinta-customers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
